1e036b11745e82c7d76eb1cb56dad66d24e80890
Mehreen Butt <mbutt@republiccharterschools.org>
2018-07-11 19:18:57 +0000

    New

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+require './config/environment'
+require './app/models/sample_model'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+
+end
---
+++ app/models/sample_model.rb
@@ -0,0 +1,4 @@
+class Review
+  attr_accessor :objective, :review_question, :answer
+
+end
